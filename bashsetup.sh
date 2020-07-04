b13089d52cd43a510e6be903a12c60335a2892e6
Zwerb LLC <zwerbllc@Zwerbs-MBP.fios-router.home>
2020-07-03 21:31:18 -0400

    making bash setup generic for unix/bash

---
+++ bashsetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing Environment for Python3 Jupyterlab Stock Analysis"
+
+pip3 install --upgrade pip
+
+pip3 install yfinance --upgrade
+pip3 install numpy --upgrade
+pip3 install matplotlib --upgrade
+pip3 install seaborn --upgrade
+pip3 install pandas --upgrade
+pip3 install yahoofinancials --upgrade
+pip3 install sklearn --upgrade
