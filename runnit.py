a3e7a59f16ae4cf9490208865e084d383c4c1c53
Zwerb LLC <zwerbllc@Zwerb.fios-router.home>
2020-07-06 22:57:44 -0400

    runnit variable input

--- runnit.py
+++ runnit.py
@@ -15,4 +15,6 @@ SPLITSDIR = 'split_files'
 
 import sa_functions as sa
 
-sa.online_process_stock_once('TSLA', 120)
+ticker = raw_input("Type a ticker: ")
+days_range = raw_input("Input days: ") 
+sa.online_process_stock_once(str(ticker), int(days_range))
